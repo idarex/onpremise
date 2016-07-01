180497f34c353ebc260c28875b6ecbad6e2eae00
dcb9 <jt1dcb@163.com>
2016-07-01 14:13:53 +0800

    Add Github and Slack Plugins

--- sentry.conf.py
+++ sentry.conf.py
@@ -289,3 +289,6 @@ if 'SENTRY_RUNNING_UWSGI' not in os.environ and len(secret_key) < 32:
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 SENTRY_OPTIONS['system.secret-key'] = secret_key
+GITHUB_APP_ID = env('GITHUB_APP_ID')
+GITHUB_API_SECRET = env('GITHUB_API_SECRET')
+GITHUB_EXTENDED_PERMISSIONS = ['repo']
